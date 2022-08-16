f1a9aeaa3564935b399e95f2ae32fcdce91f073d
applan <applan_siepe@naver.com>
2022-08-17 03:05:41 +0900

    2022-08-17 API_URL 추가

--- src/consts/Consts.tsx
+++ src/consts/Consts.tsx
@@ -4,4 +4,9 @@
 export const PROJECT_REGEX = {
     // https://www.w3resource.com/javascript/form/email-validation.php
     EMAIL : /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-}
+};
+
+/**
+ * 프로젝트 API 통신 URL 주소
+ */
+export const PROJECT_API_URL = "http://localhost:8080";
