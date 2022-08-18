ce7ac8705cd8ced41bfa5d564e7b07cd0bb7ebd3
applan <applan_siepe@naver.com>
2022-08-19 05:49:22 +0900

    2022-08-19 기본 목록 페이지 추가

--- src/pages/Routers.tsx
+++ src/pages/Routers.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from "./auth/Auth";
+import Todo from "./todo/Todo";
 
 /**
  * 페이지 이동을 처리하는 Routers 생성
@@ -9,6 +10,7 @@ export const Routers = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Todo/>}></Route>
                 <Route path="/auth" element={<Auth/>}></Route>
             </Routes>
         </BrowserRouter>
