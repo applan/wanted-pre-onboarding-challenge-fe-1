55439ffa25ed0f1748a197b9003357858c36ab7c
applan <applan_siepe@naver.com>
2022-08-22 01:01:01 +0900

    2022-08-22 TodoList, TodoForm 컴포넌트로 변경, TodoList useState 추가

--- src/pages/todo/Todo.tsx
+++ src/pages/todo/Todo.tsx
@@ -1,13 +1,19 @@
-import {useEffect} from "react";
-import {Card, Nav, Navbar, Container, ListGroup, Form, Button } from "react-bootstrap";
-import sideImage from "../../images/sideImage-Onboarding.png";
+import {useEffect, useState} from "react";
+import {Nav, Navbar, Container} from "react-bootstrap";
 import myProfile from "../../images/myProfile.jpg";
 import {useNavigate} from "react-router-dom";
+import TodoForm from "../../components/TodoForm";
+import TodoList from "../../components/TodoList";
+import {TodoData} from "../../types/Types";
+
 
 export const Todo = () => {
 
     let navigate = useNavigate();
+    const [userState, setUserState] = useState<string>("");
+    const [todoList, setTodoList] = useState<TodoData[]>([]);
     useEffect(() => {
+        setUserState('ADD');
         const token = localStorage.getItem("token");
         if(token == "" || token == "null" || token == null) navigate("/auth");
     });
@@ -18,13 +24,6 @@ export const Todo = () => {
     const handleLogout = () => {
         localStorage.removeItem("token");
         navigate("/auth");
-    }
-
-    /**
-     * 추후 Todo_element 선택 시 값 값 자세히 보여주는 함수
-     */
-    const handleTodoClick = () => {
-        console.log('으엉');
     }
 
     return (
@@ -48,68 +47,8 @@ export const Todo = () => {
                     </Container>
                 </Navbar>
                 <div className="row h-80 w-100">
-                    <div className="col-3 h-100">
-                        <Card>
-                            <Card.Img variant="top" src={sideImage}/>
-                            <Card.Body>
-                                <Form>
-                                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                                        <Form.Label>제목</Form.Label>
-                                        <Form.Control type="text" placeholder="제목을 입력해주세요" />
-                                    </Form.Group>
-
-                                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                                        <Form.Label>내용</Form.Label>
-                                        <Form.Control as="textarea" rows={3} />
-                                    </Form.Group>
-                                    <div className="w-100 text-end">
-                                        <Button variant="primary" type="submit">
-                                            추가
-                                        </Button>
-                                    </div>
-                                </Form>
-                            </Card.Body>
-                            <Card.Header className="border-top">저장된 TODO_</Card.Header>
-                            <ListGroup id="myTodoListBox" className="list-group-flush">
-                                <ListGroup.Item action onClick={handleTodoClick}>
-                                    <div className="ms-2 me-auto">
-                                        <div className="fw-bold">등장!</div>
-                                        dkdkdkdkaskdsakdl;sakd;lsadsad
-                                    </div>
-                                </ListGroup.Item>
-                                <ListGroup.Item action onClick={handleTodoClick}>
-                                    <div className="ms-2 me-auto">
-                                        <div className="fw-bold">등장!</div>
-                                        dkdkdkdkaskdsakdl;sakd;lsadsad
-                                    </div>
-                                </ListGroup.Item>
-                            </ListGroup>
-                        </Card>
-                    </div>
-                    <div className="col-9 h-70 border rounded-sm" >
-                        <Form.Group className="mb-3">
-                            <Form.Label>Disabled input</Form.Label>
-                            <Form.Control placeholder="Disabled input" disabled />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                            <Form.Label>내용</Form.Label>
-                            <Form.Control className="resize-none" as="textarea" rows={19} disabled/>
-                        </Form.Group>
-                        <div className="w-100 text-end">
-                            <Button className="me-2 " variant="warning" type="submit">
-                                수정
-                            </Button>
-                            <Button className="me-2 " variant="primary" type="submit">
-                                저장
-                            </Button>
-                            <Button className="me-2" variant="secondary" type="submit">
-                                취소
-                            </Button>
-                            <Button variant="danger" type="submit">
-                                삭제
-                            </Button>
-                        </div>
-                    </div>
+                    <TodoList/>
+                    <TodoForm userState={userState}/>
                 </div>
             </main>
         </section>
