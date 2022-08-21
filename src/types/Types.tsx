4b1f7459f1497f04de7057ec5e92a9ec198ba13a
applan <applan_siepe@naver.com>
2022-08-22 01:01:49 +0900

    2022-08-22 Todo 데이터를 위한 Type 선언

--- src/types/Types.tsx
+++ src/types/Types.tsx
@@ -1,7 +1,15 @@
 /**
- * login, signIn에 사용하는 변수
+ * login, signIn에 사용하는 타입
  */
 export type User = {
     email?: string | undefined;
     password?: string | undefined;
+}
+
+/**
+ * Todo_를 위한 타입
+ */
+export type TodoData = {
+    title: string | undefined;
+    contents: string | undefined;
 }
